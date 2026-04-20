9523ffef02f684780a3f5571c9bf88ea538b9f44
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-20 13:22:26 +0400

    create a HashMap named indexMap

--- src/AllSwap.java
+++ src/AllSwap.java
@@ -4,21 +4,26 @@ public class AllSwap {
     public static void main(String[] args) {
 
 
-
     }
 
     public static String[] allSwap(String[] strings) {
 
+        Map<Character, Integer> indexMap = new HashMap<>();
+
         for (int i = 0; i < strings.length; i++) {
 
+            char key = strings[i].charAt(0);
 
+            if (indexMap.containsKey(key)) {
+                int prevIndex = indexMap.get(key);
+
+
+
+            }
 
 
         }
 
 
-
     }
 
-
-    }
