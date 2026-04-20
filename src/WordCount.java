33ce0e69f5440f3595f0da1ed197e232ce0f4ae5
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-20 11:44:53 +0400

    Use an if else with containsKey()method to check if the input is already in the map and based on that determine the value of HashMap

--- src/WordCount.java
+++ src/WordCount.java
@@ -5,13 +5,17 @@ public class WordCount {
 
         String[] input = {"a", "b", "a", "c", "b"};
         Map<String, Integer> result = new HashMap<>();
+
         for (String in : input) {
 
+            if (result.containsKey(in)) {
+                result.put(in, result.get(in) + 1);
+            } else {
+
+                result.put(in, 1);
+            }
 
     }
-
-
-
 
 }
 
