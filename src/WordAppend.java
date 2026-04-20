8d28ed7e3d44710e9f6888a55387af183ddaa875
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-20 12:38:01 +0400

    Create a String variable named result+ loop throgh array items+ add an if els to check if key is exist and based on that determine the value of HashMap+Call the function in main method

--- src/WordAppend.java
+++ src/WordAppend.java
@@ -2,17 +2,33 @@ import java.util.*;
 public class WordAppend {
     public static void main(String[] args) {
 
+        String[] input = {"a", "b", "a"};
 
+        System.out.println(wordAppend(input));
 
     }
 
-    public static String wordAppend(String[] words) {
+    public static String wordAppend(String[] strings) {
 
         Map<String, Integer> countMap = new HashMap<>();
 
+        String result = "";
 
+        for (String str : strings) {
 
+            if (countMap.containsKey(str)) {
 
+                countMap.put(str, countMap.get(str) + 1);
+            }else{
+                countMap.put(str, 1);
+            }
+
+            if (countMap.get(str) % 2 == 0) {
+                result+= str;
+
+            }
+        }
+        return result;
     }
 
     }
