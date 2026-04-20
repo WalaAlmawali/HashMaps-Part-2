e20c6e9fbfca71b3743cdf4448e8bef4e122d80b
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-20 15:57:50 +0400

    Call firstSwap function in tha main method

--- src/FirstSwap.java
+++ src/FirstSwap.java
@@ -5,16 +5,47 @@ public class FirstSwap {
     public static void main(String[] args) {
 
 
+        String[] input = {"ax", "bx", "cx", "cy", "by", "ay", "aaa", "azz"};
 
-    }
-
-    public static String[] allSwap(String[] strings) {
-
-
-
-
+        System.out.println(java.util.Arrays.toString(firstSwap(input)));
 
 
     }
+    public static String[] firstSwap(String[] strings) {
 
+        Map<String, Integer> IndexMap = new HashMap<>();
+
+        Set<String> disabled = new HashSet<>();
+
+        int i = 0;
+
+        for (String x : strings) {
+
+            String target = x.substring(0, 1);
+
+            if (IndexMap.containsKey(target) && !(disabled.contains(target)) ) {
+
+                int prevIndex = IndexMap.get(target);
+                String temp = strings[i];
+                strings[i] = strings[prevIndex];
+                strings[prevIndex] = temp;
+
+                disabled.add(target);
+
+                i++;
+
+            } else {
+
+                IndexMap.put(target, i);
+
+                i++;
+            }
+
+        }
+
+
+        return strings;
     }
+
+
+}
