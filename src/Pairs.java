ad341b1bd9e38004ed2ced1d8d5759df628cde1f
Wala AL mawali <wala.salam.9888@gmail.com>
2026-04-20 11:31:49 +0400

    Inside the loop add firstChar as a key and lastChar as a value of result HashMap + chang HashMape type to <String, String>

--- src/Pairs.java
+++ src/Pairs.java
@@ -4,12 +4,14 @@ public class Pairs {
     public static void main(String[] args) {
 
         String[] pairs = {"code", "bug"};
-        Map<String, Integer> result = new HashMap<>();
+        Map<String, String> result = new HashMap<>();
 
         for (String pair : pairs) {
 
             String firstChar = pair.substring(0,1);
             String lastChar = pair.substring(pair.length() - 1);
+
+            result.put(firstChar, lastChar);
 
 
         }
